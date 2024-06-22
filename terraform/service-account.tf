f4de7ec2da81b686297dcf7c8f9e8865f223ef5e
Keroles-Nadyy <Keroles_N@outlook.com>
2024-06-22 19:01:06 +0300

    added kubernetes-cluster-role

--- terraform/service-account.tf
+++ terraform/service-account.tf
@@ -31,3 +31,30 @@ resource "kubernetes_secret" "jenkins_token" {
   }
   type = "kubernetes.io/service-account-token"
 }
+
+resource "kubernetes_cluster_role" "jenkins-admin" {
+  metadata {
+    name = "jenkins-admin"
+  }
+  rule {
+    api_groups = [""]
+    resources  = ["*"]
+    verbs      = ["*"]
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "jenkins-cluster-role-binding" {
+  metadata {
+    name = "jenkins-admin"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "jenkins-admin"
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_cluster_role.jenkins-admin.metadata[0].name
+    namespace = "tools"
+  }
+}
